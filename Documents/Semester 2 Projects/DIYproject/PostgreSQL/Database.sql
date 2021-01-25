3df5b4948cd9fa7e1e63973e46f6787c10f490bf
Marija Bolic <mboli027@uottawa.ca>
2021-01-24 19:53:05 -0500

    created Recipes table and added constraints

--- Documents/Semester 2 Projects/DIYproject/PostgreSQL/Database.sql
+++ Documents/Semester 2 Projects/DIYproject/PostgreSQL/Database.sql
@@ -1,7 +1,19 @@
 
 /* Created User Table */
 CREATE TABLE Users ( 
-UserID SERIAL, FirstName VARCHAR(20) not null, LastName VARCHAR(20) not null, 
-Email VARCHAR(30) not null, IsPremiumMember BOOLEAN not null, 
+UserID SERIAL, FirstName VARCHAR(20) NOT null, LastName VARCHAR(20) NOT null, 
+Email VARCHAR(30) NOT null, IsPremiumMember BOOLEAN NOT null, 
 PRIMARY KEY (UserID) 
+);
+
+/* Created User Table */ 
+CREATE TABLE Recipes ( 
+RecipeID SERIAL, CreatedDate DATE NOT null, LastUpdatedDate DATE NOT null DEFAULT CURRENT_DATE,
+Author INTEGER NOT null, RecipeContent VARCHAR(4000) NOT null, Category VARCHAR(20), 
+IsPrivate BOOLEAN NOT null DEFAULT true, IsPremium BOOLEAN NOT null NOT NULL DEFAULT false, 
+IsDeleted BOOLEAN NOT null,
+PRIMARY KEY (RecipeID),
+CONSTRAINT fk_author
+   FOREIGN KEY(Author) 
+   REFERENCES Users(UserID)
 );
